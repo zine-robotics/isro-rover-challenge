19315fc62a4c491d5acd1886332116edffbcafdc
AswinArun <aswinarunkumar8@gmail.com>
2024-06-22 00:34:28 +0300

    removed moveit from arm control

--- arm/arm_planner/arm_planner/adding_gripper.py
+++ arm/arm_planner/arm_planner/adding_gripper.py
@@ -255,7 +255,7 @@ def plan_and_execute(joint_names, joint_values, logger, sleep_time=0.0):
     time.sleep(sleep_time)
 
     
-def move_to_pose(logger,pose):
+def move_to_pose(logger,pose, gripper_close=0):
     
     # zine_arm.set_start_state_to_current_state()
     # robot_model = zine_rover.get_robot_model()
@@ -266,13 +266,14 @@ def move_to_pose(logger,pose):
     z = pose['z']
 
     joint_ik_values = zine_ik_solver(x,y,z)
-    joint_names = ["joint_0", "joint1", "joint2", "joint3", "joint4"]
+    joint_names = ["joint_0", "joint1", "joint2", "joint3", "joint4", "joint5"]
     joint_values = [
         joint_ik_values[0],
         joint_ik_values[1],
         joint_ik_values[2],
         joint_ik_values[3],
         joint_ik_values[4],
+        gripper_close
     ]
 
     # robot_state.joint_positions = joint_values
@@ -292,19 +293,20 @@ def pick_object(logger,pose):
 
     #implement approcah near object
     pose['x']-=0.05
-    joint_names, joint_values = move_to_pose(logger,pose)
+    joint_names, joint_values = move_to_pose(logger,pose,gripper_close=0.0)
     logger.info("Arm reached for approaching position")
 
     #implment approach at object
     pose['x']+=0.05
-    joint_names, joint_values = move_to_pose(logger,pose)
+    joint_names, joint_values = move_to_pose(logger,pose,gripper_close=0.0)
     logger.info("Arm reached for gripping position")
-
     #implement gripping action 
 
     #implement move to ready state
     # zine_arm.set_start_state_to_current_state()
     # zine_arm.set_goal_state(configuration_name="ready")
+    
+    joint_values[5] = 1.0 # close gripper
     
     plan_and_execute( joint_names, joint_values, logger, sleep_time=5)
     logger.info("Arm reached ready state")
@@ -314,19 +316,22 @@ def drop_object(logger,pose):
 
     #implement approcah near object
     pose['x']-=0.05
-    move_to_pose(logger,pose)
+    joint_names, joint_values = move_to_pose(logger,pose, gripper_close=1.0)
     logger.info("Arm reached for approaching position")
 
     #implment approach at object
     pose['x']+=0.05
-    move_to_pose(logger,pose)
+    joint_names, joint_values = move_to_pose(logger,pose, gripper_close=1.0)
     logger.info("Arm reached for gripping position")
 
     #implement dropping action 
+    joint_values[5] = 0.0 # open gripper
+    plan_and_execute( joint_names, joint_values, logger, sleep_time=5)
 
     #implement move to ready state
     # zine_arm.set_start_state_to_current_state()
     # zine_arm.set_goal_state(configuration_name="ready")
+    
 
 
 def main():
